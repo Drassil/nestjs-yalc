7479b5fb36af8fd456cef1ea67856e0b823c0815
Yehonal <giuseppe@bitvavo.com>
2022-04-04 13:40:31 +0000

    feat: merge enum utils

--- types/src/globals.d.ts
+++ types/src/globals.d.ts
@@ -22,3 +22,26 @@ export declare type AnyConstructor<A = Record<string, unknown>> = new (
 ) => A;
 
 export declare type Mixin<T extends AnyFunction> = InstanceType<ReturnType<T>>;
+
+type OptionalPropertyNames<T> = {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  [K in keyof T]-?: {} extends Pick<T, K> ? K : never;
+}[keyof T];
+
+type SpreadProperties<L, R, K extends keyof L & keyof R> = {
+  [P in K]: L[P] | Exclude<R[P], undefined>;
+};
+
+type Id<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
+
+// Type of { ...L, ...R }
+type Spread<L, R> = Id<
+  // Properties in L that don't exist in R
+  Pick<L, Exclude<keyof L, keyof R>> &
+    // Properties in R with types that exclude undefined
+    Pick<R, Exclude<keyof R, OptionalPropertyNames<R>>> &
+    // Properties in R, with types that include undefined, that don't exist in L
+    Pick<R, Exclude<OptionalPropertyNames<R>, keyof L>> &
+    // Properties in R, with types that include undefined, that exist in L
+    SpreadProperties<L, R, OptionalPropertyNames<R> & keyof L>
+>;
---
+++ utils/src/__mocks__/enum.mock.ts
@@ -0,0 +1,9 @@
+export enum TestEnum1 {
+  TEST1 = 'test1',
+  TEST2 = 'test2',
+}
+
+export enum TestEnum2 {
+  TEST2 = 'test2',
+  TEST3 = 'test3',
+}
