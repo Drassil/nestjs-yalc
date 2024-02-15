944e3b75ed1b93bba632ef33aa93b41174e5034b
Yehonal <joseph@fung.money>
2024-02-15 01:58:57 +0100

    fix(dataloader): another attempt

--- data-loader/src/dataloader.helper.ts
+++ data-loader/src/dataloader.helper.ts
@@ -18,7 +18,7 @@ import {
 import { ClassType } from '@nestjs-yalc/types/globals.d.js';
 import { getProviderToken } from '@nestjs-yalc/crud-gen/crud-gen.helpers.js';
 import { EventCrudGen } from '@nestjs-yalc/crud-gen/event.enum.js';
-import eventemitter2, { type EventEmitter2 } from 'eventemitter2';
+import { EventEmitter2 as EE2, type EventEmitter2 } from 'eventemitter2';
 
 export type SearchKeyType<E, T = string> = [keyof E, T] | T | undefined;
 
@@ -292,10 +292,7 @@ export function DataLoaderFactory<Entity extends Record<string, any>>(
         eventEmitter,
       );
     },
-    inject: [
-      serviceToken ?? getServiceToken(entity),
-      eventemitter2.EventEmitter2,
-    ],
+    inject: [serviceToken ?? getServiceToken(entity), EE2],
     scope: Scope.REQUEST,
   };
 }
