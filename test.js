40e2fb388c9871201f2018a6d63a2c3a0f138f64
Yehonal <yehonal.azeroth@gmail.com>
2024-02-15 19:39:47 +0100

    fix: improvements after tests (#38)

--- test.js
+++ test.js
@@ -9,14 +9,23 @@ import * as url from 'url';
 // const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-const newArgs = process.argv.slice(2).filter(
-  (p) =>
-    ![
-      '--testPathPattern', // we want to treat the path by using the parser of our jest.config.ts
-      // '--findRelatedTests', // this was not letting jest to generate coverage, but apparently it is not used anymore by vscode
-      '--testLocationInResults', // this causes an error in vscode (to investigate)
-    ].includes(p),
-);
+const newArgs = process.argv
+  .slice(2)
+  .filter(
+    (p) =>
+      ![
+        '--testPathPattern', // we want to treat the path by using the parser of our jest.config.ts
+        // '--findRelatedTests', // this was not letting jest to generate coverage, but apparently it is not used anymore by vscode
+        '--testLocationInResults', // this causes an error in vscode (to investigate)
+      ].includes(p),
+  )
+  .map((p) => {
+    if (!p.startsWith('--')) {
+      return `"${p}"`; // we need to wrap the argument values with quotes to avoid issues with spaces
+    }
+
+    return p;
+  });
 
 var command = 'npm';
 
